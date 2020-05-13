492b7c481ee933f28fedf5a2763146d9d8dce3f0
Simon Wicky <simon.wicky@epfl.ch>
2020-05-13 10:57:12 +0200

    fix fuzzer_load_png

--- lab04/libfuzzer/src/fuzzer_load_png.c
+++ lab04/libfuzzer/src/fuzzer_load_png.c
@@ -1,18 +1,23 @@
 #include "pngparser.h"
 #include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
 
 // LibFuzzer stub
 //
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size) {
   struct image *test_img;
 
-  FILE *input = fopen("testfile.png","w");
+  pid_t pid = getpid();
+  char filename[18];
+  sprintf(filename,"testfile%d.png", pid);
+  FILE *input = fopen(filename,"w");
   fwrite(Data, Size, 1, input);
   fclose(input);
   
   // What would happen if we run multiple fuzzing processes at the same time?
   // Take a look at the name of the file.
-  load_png("testfile.png", &test_img);
+  load_png(filename, &test_img);
 
   // Always return 0
   return 0;
