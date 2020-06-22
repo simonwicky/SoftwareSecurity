6614aa26ef7ffed75dfa3dcb1cb2814121e4d1bc
Simon Wicky <simon.wicky@epfl.ch>
2020-06-22 18:01:17 +0200

    report and patch bug 5

--- exam/grass/src_patched/user/user.cpp
+++ exam/grass/src_patched/user/user.cpp
@@ -85,7 +85,7 @@ void UserState::getUser(string &name) {
 
 void UserState::makeUserFile() {
   char buf[128];
-  sprintf(buf, "%s/%sXXXXXX", USERS_FILE_DIR, userName.c_str());
+  sprintf(buf, "%s/%sXXXXXX", USERS_FILE_DIR, userName.substr(0,100).c_str());
   /* Create users file if it does not exist */
   int userFileFD = mkstemp(buf);
   assert (userFileFD != -1);
