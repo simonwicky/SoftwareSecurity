98b6e1f6aeba3d126c7c31b1e6a9acea6bf87d3c
Simon Wicky <simon.wicky@epfl.ch>
2020-06-24 12:48:09 +0200

    add bug 3 and 4

--- exam/regex/src/parse.cpp
+++ exam/regex/src/parse.cpp
@@ -42,6 +42,9 @@ static RegularExpression * try_parse_range(const char ** str, size_t *len, Regul
                 range = true;
                 next_state = STATE_PARSE_MAX;
             } else if (c >= '0' && c <= '9') {
+                if (min > UINT_MAX / 10) {
+                    goto error; //will overflow
+                }
                 min = min * 10 + (c - '0');
             } else if (c == '}') {
                 next_state = STATE_TRY_PARSE_LAZY; // parsing complete
@@ -55,6 +58,9 @@ static RegularExpression * try_parse_range(const char ** str, size_t *len, Regul
                 if (!max_available) {
                     max = 0;
                     max_available = true;
+                }
+                if (max > UINT_MAX / 10) {
+                    goto error; //will overflow
                 }
                 max = max * 10 + (c - '0');
             } else if (c == '}') {
