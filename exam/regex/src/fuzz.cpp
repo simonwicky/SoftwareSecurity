992a031f66ab42b82dbe1992733a1c2f79d45027
Simon Wicky <simon.wicky@epfl.ch>
2020-06-24 12:48:57 +0200

    change fuzz method

--- exam/regex/src/fuzz.cpp
+++ exam/regex/src/fuzz.cpp
@@ -2,7 +2,7 @@
 #include <stdint.h>
 #include <string.h>
 
-#define REG_LEN 16
+#define REG_LEN 128
 
 extern "C"
 int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
@@ -47,7 +47,31 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
      *   your fuzzing process as it only encounters crashes for the remaining
      *   bugs.
      */
+//     if (Size == 0) {
+//          return 0;
+//     }
+//     uint8_t *length = (uint8_t *)Data;
 
+//      if(Size < *length + 1) {
+//           return 0;
+//      }
+
+//      char regex[*length + 1];
+//      memcpy(regex, &Data[1], *length);
+//      regex[*length] = '\0';
+
+//      char text[Size - *length];
+//      memcpy(text, &Data[*length + 1], Size - *length - 1);
+//      text[Size - *length - 1] = '\0';
+
+//      int err = 0;
+//      RegularExpression *r = parse(regex, strlen(regex), &err);
+//      if(err != 0) {
+//           return 0;
+//      }
+//      SearchExpression s(r);
+//      s.search(text, strlen(text));
+//      return 0;
      if(Size < REG_LEN) {
           return 0;
      }
@@ -71,18 +95,18 @@ int LLVMFuzzerTestOneInput(const uint8_t *Data, size_t Size)
 
 }
 
-int main(int argc, char *argv[]){
+// int main(int argc, char *argv[]){
 
-     FILE* f = fopen(argv[1],"r");
-     size_t size;
-     fseek(f, 0L, SEEK_END);
-     size = ftell(f);
-     fseek(f, 0L, SEEK_SET);
-     uint8_t buf[size];
-     fread(buf, sizeof(uint8_t), size, f);
-     fclose(f);
+//      FILE* f = fopen(argv[1],"r");
+//      size_t size;
+//      fseek(f, 0L, SEEK_END);
+//      size = ftell(f);
+//      fseek(f, 0L, SEEK_SET);
+//      uint8_t buf[size];
+//      fread(buf, sizeof(uint8_t), size, f);
+//      fclose(f);
 
-     buf[size] = 0;
+//      buf[size] = 0;
 
-     return LLVMFuzzerTestOneInput(buf, size);
-}
+//      return LLVMFuzzerTestOneInput(buf, size);
+// }
